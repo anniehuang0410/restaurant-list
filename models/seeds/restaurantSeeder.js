c77a4abfc6af6fe6a4db9ce5ce4c829b8fdc480f
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-17 18:14:26 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,67 @@
-const Restaurant = require('../restaurant')
-
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 // require restaurant data
+const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json')
+const restaurant = restaurantList.results
+
+// User seeds
+const User = require('../user')
+// 依題目規定：user1 -> 1-3 res list; user1 -> 4-6 res list 
+const SEED_USER = [
+  {
+    name: 'User1',
+    email: 'user1@example.com',
+    password: '12345678',
+    startIndex: 0,
+    endIndex: 3
+  }, 
+  {
+    name: 'User2',
+    email: 'user2@example.com',
+    password: '12345678',
+    startIndex: 3,
+    endIndex: 6
+  }
+]
 
 const db = require('../../config/mongoose')
+
 db.once('open', () => {
-  console.log('MondoDB connected.')
-  const restaurants = restaurantList.results
-  for (let i = 0; i < restaurants.length; i++) {
-    Restaurant.create({ 
-      name: restaurants[i].name,
-      name_en: restaurants[i].name_en,
-      category: restaurants[i].category,
-      image: restaurants[i].image,
-      location: restaurants[i].location,
-      phone: restaurants[i].phone,
-      google_map: restaurants[i].google_map,
-      rating: restaurants[i].rating,
-      description: restaurants[i].description 
-    })
-  }
-})
+  Promise.all(
+    SEED_USER.map(user => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(user.password, salt))
+        .then(hash => User.create({
+          name: user.name,
+          email: user.email,
+          password: hash,
+        }))
+      .then(seedUser => {
+        const userId = seedUser._id
+        const userRestaurants = restaurant.slice(user.startIndex, user.endIndex) 
+        return Promise.all(userRestaurants.map(restaurant => 
+          Restaurant.create({
+            id: restaurant.id,
+            name: restaurant.name,
+            name_en: restaurant.name_en,
+            category: restaurant.category,
+            image: restaurant.image,
+            location: restaurant.location,
+            phone: restaurant.phone,
+            google_map: restaurant.google_map,
+            rating: restaurant.rating,
+            description: restaurant.description,
+            userId
+          })
+        ))
+      })
+      .then(() => {
+        console.log('done!')
+        process.exit()
+      })
+      .catch(err => console.log(err))
+}))})
